86efd8fe32826567f7210d075d8eaa8c101b2323
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-06-01 10:48:05 +0300

    'Test'

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,70 +1,109 @@
-import React from 'react';
-import './style.css';
-import ImageUploader from '../ImageUploader/ImageUploader';
+import React from "react";
+import "./style.css";
 
-class Modal extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			isOpen: true
-		};
-		this.toggle = this.toggle.bind(this);
-		this.onFieldChange = this.onFieldChange.bind(this);
-		this.onFormSubmit = this.onFormSubmit.bind(this);
-	}
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    };
+    this.toggle = this.toggle.bind(this);
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.handlePhotoUpload = this.handlePhotoUpload.bind(this);
+  }
 
-	toggle() {
-		this.setState(prevState => ({
-			isOpen: !prevState.isOpen
-		}));
-		
-	}
+  toggle() {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  }
 
-	onFieldChange(event){
-		console.log(this);
-		const fieldName = event.target.name;
+  onFieldChange(event) {
+    const fieldName = event.target.name;
     const fieldValue = event.target.value;
     this.props.onChange(fieldName, fieldValue);
-	}
+  }
 
-	onFormSubmit(e) {
-		this.props.onSubmit(e);
-	}
+  onFormSubmit(event) {
+    this.props.onSubmit(event);
+  }
 
-	render() {
-		const modal = this.state.isOpen ? 
-					(
-						<div className='modal' >
-							<form onSubmit={this.onFormSubmit}>
-						  	<div className="inputsContainer">
-								  	<ImageUploader />
-									    <div className="recipeInputs">
-									      <label>Recipe Name</label>
-													<input name="recipeName" type="text" onChange={this.onFieldChange}/>
-													<label>Cook Time</label>
-													<input name="cookTime" type="time" step="1" onChange={this.onFieldChange} />
-													<label>Ingredients</label>
-													<textarea name="ingredients" placeholder="Put each ingredient on its own line"></textarea>
-													<label>Directions</label>
-													<textarea name="directions" placeholder="Put each step on its own line" onChange={this.onFieldChange}></textarea>
-									    </div>
-										</div>
-										 <div className="formBtns">
-										   <input type="reset" />
-										   <input type="submit" />
-									   </div>
-						    </form>
-						</div>
-					)
-					:
-					'';
-		return(
-			<div>
-				<button onClick={this.toggle}>Recipe</button>
-				{modal}
-			</div>
-		)
-	}
+  handlePhotoUpload(event) {
+    const imageContainer = document.querySelector(".imageContainer");
+    const label = document.querySelector(".imageContainer label");
+    const file = URL.createObjectURL(event.target.files[0]);
+
+    imageContainer.style.backgroundImage = `url(${file})`;
+    imageContainer.style.backgroundSize = "100%";
+    imageContainer.style.backgroundRepeat = "no-repeat";
+    imageContainer.style.backgroundPosition = "center";
+    label.style.display = "none";
+
+    this.props.onPhotoUpload(event, file);
+  }
+
+  render() {
+    const modal = this.state.isOpen ? (
+      <div className="modal">
+        <form onSubmit={this.onFormSubmit}>
+          <div className="inputsContainer">
+            <div>
+              <span>Recipe Photo</span>
+              <div className="imageContainer">
+                <label htmlFor="imageUpload" className="fas fa-upload" />
+                <input
+                  id="imageUpload"
+                  type="file"
+                  accept="image/*"
+                  onChange={this.handlePhotoUpload}
+                />
+              </div>
+            </div>
+            <div className="recipeInputs">
+              <label>Recipe Name</label>
+              <input
+                name="recipeName"
+                type="text"
+                onChange={this.onFieldChange}
+              />
+              <label>Cook Time</label>
+              <input
+                name="cookTime"
+                type="time"
+                step="1"
+                onChange={this.onFieldChange}
+              />
+              <label>Ingredients</label>
+              <textarea
+                name="ingredients"
+                placeholder="Put each ingredient on its own line"
+                onChange={this.onFieldChange}
+              />
+              <label>Directions</label>
+              <textarea
+                name="directions"
+                placeholder="Put each step on its own line"
+                onChange={this.onFieldChange}
+              />
+            </div>
+          </div>
+          <div className="formBtns">
+            <input type="reset" />
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    ) : (
+      ""
+    );
+    return (
+      <div>
+        <button onClick={this.toggle}>Recipe</button>
+        {modal}
+      </div>
+    );
+  }
 }
 
 export default Modal;
--- src/Components/RecipeSaver.js
+++ src/Components/RecipeSaver.js
@@ -1,95 +1,125 @@
-import React from 'react';
-import RecipesRender from './RecipesRender/RecipesRender';
-import Modal from './Modal/Modal';
-import './global.css';
-
+import React from "react";
+import RecipesRender from "./RecipesRender/RecipesRender";
+import Modal from "./Modal/Modal";
+import "./global.css";
 
 class RecipeSaver extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			recipeName: '',
-			cookTime:'',
-			ingredients:'',
-			directions:'',
-			dishImg:'',
-			recipes: [
-				{	
-					recipeName: 'Ham And Cheese Breakfast Pockets',
-					cookTime: 10,
-					ingredients: ['Prod 1', 'Prod 2', 'Prod 3'],
-					directions: ['Frist Direction', 'Second Direction', 'Third Direction'],
-					dishImg: 'https://loremflickr.com/320/240?random=1'
-				},
-				{	
-					recipeName: '3 Meals, 1 Snack, $10 (7-Day Make-Ahead Meal Plan Day 5)',
-					cookTime: 60,
-					ingredients: ['Prod 1', 'Prod 2', 'Prod 3'],
-					directions: ['Frist Direction', 'Second Direction', 'Third Direction'],
-					dishImg: 'https://loremflickr.com/320/240?random=2'
-				},
-				{	
-					recipeName: 'Ramadan Dishes for Suhoor & Iftar',
-					cookTime: 50,
-					ingredients: ['Prod 1', 'Prod 2', 'Prod 3'],
-					directions: ['Frist Direction', 'Second Direction', 'Third Direction'],
-					dishImg: 'https://loremflickr.com/320/240?random=3'
-				},
-				{	
-					recipeName: '3-Ingredient Cookies ‘n’ Cream Ice Cream',
-					cookTime: 50,
-					ingredients: ['Prod 1', 'Prod 2', 'Prod 3'],
-					directions: ['Frist Direction', 'Second Direction', 'Third Direction'],
-					dishImg: 'https://loremflickr.com/320/240?random=4'
-				},
-					{	
-					recipeName: 'Cheesy Eggplant Roll-Ups',
-					cookTime: 50,
-					ingredients: ['Prod 1', 'Prod 2', 'Prod 3'],
-					directions: ['Frist Direction', 'Second Direction', 'Third Direction'],
-					dishImg: 'https://loremflickr.com/320/240?random=5'
-				},
-			]
-		}
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipeName: "",
+      cookTime: "",
+      ingredients: "",
+      directions: "",
+      dishImg: null,
+      recipes: [
+        {
+          recipeName: "Ham And Cheese Breakfast Pockets",
+          cookTime: 10,
+          ingredients: ["Prod 1", "Prod 2", "Prod 3"],
+          directions: [
+            "Frist Direction",
+            "Second Direction",
+            "Third Direction"
+          ],
+          dishImg: "https://loremflickr.com/320/240?random=1"
+        },
+        {
+          recipeName:
+            "3 Meals, 1 Snack, $10 (7-Day Make-Ahead Meal Plan Day 5)",
+          cookTime: 60,
+          ingredients: ["Prod 1", "Prod 2", "Prod 3"],
+          directions: [
+            "Frist Direction",
+            "Second Direction",
+            "Third Direction"
+          ],
+          dishImg: "https://loremflickr.com/320/240?random=2"
+        },
+        {
+          recipeName: "Ramadan Dishes for Suhoor & Iftar",
+          cookTime: 50,
+          ingredients: ["Prod 1", "Prod 2", "Prod 3"],
+          directions: [
+            "Frist Direction",
+            "Second Direction",
+            "Third Direction"
+          ],
+          dishImg: "https://loremflickr.com/320/240?random=3"
+        },
+        {
+          recipeName: "3-Ingredient Cookies ‘n’ Cream Ice Cream",
+          cookTime: 50,
+          ingredients: ["Prod 1", "Prod 2", "Prod 3"],
+          directions: [
+            "Frist Direction",
+            "Second Direction",
+            "Third Direction"
+          ],
+          dishImg: "https://loremflickr.com/320/240?random=4"
+        },
+        {
+          recipeName: "Cheesy Eggplant Roll-Ups",
+          cookTime: 50,
+          ingredients: ["Prod 1", "Prod 2", "Prod 3"],
+          directions: [
+            "Frist Direction",
+            "Second Direction",
+            "Third Direction"
+          ],
+          dishImg: "https://loremflickr.com/320/240?random=5"
+        }
+      ]
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onPhotoUpload = this.onPhotoUpload.bind(this);
+  }
 
-	onChange(field, value) {
-		this.setState({
-			[field]: value
-		});
-	}
+  onChange(field, value) {
+    this.setState({
+      [field]: value
+    });
+  }
 
-	onSubmit(e) {
-		e.preventDefault();
-		const ingredients = this.state.ingredients.split(/\n/);
-		const directions = this.state.directions.split(/\n/);
-		const recipeTemplate = {
-			recipeName: this.state.recipeName,
-			cookTime: this.state.cookTime,
-			ingredients: ingredients,
-			directions: directions,
-			dishImg: this.state.dishImg
-		}
+  onPhotoUpload(field, value) {
+    this.setState({
+      dishImg: value
+    });
+  }
 
-		const recipes = this.state.recipes.slice();
-		recipes.push(recipeTemplate)
+  onSubmit(e) {
+    e.preventDefault();
+    const ingredients = this.state.ingredients.split(/\n/);
+    const directions = this.state.directions.split(/\n/);
+    const recipeTemplate = {
+      recipeName: this.state.recipeName,
+      cookTime: this.state.cookTime,
+      ingredients: ingredients,
+      directions: directions,
+      dishImg: this.state.dishImg
+    };
 
-		this.setState({
-			recipes: recipes
-		})
+    const recipes = this.state.recipes.slice();
+    recipes.push(recipeTemplate);
 
-	}
+    this.setState({
+      recipes: recipes
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				<Modal onChange={this.onChange} onSubmit={this.onSubmit} />
-				<RecipesRender recipes={this.state.recipes} />
-			</div>
-			)
-	}
+  render() {
+    return (
+      <div>
+        <Modal
+          onChange={this.onChange}
+          onSubmit={this.onSubmit}
+          onPhotoUpload={this.onPhotoUpload}
+        />
+        <RecipesRender recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
-export default RecipeSaver
+export default RecipeSaver;
