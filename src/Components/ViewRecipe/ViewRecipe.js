d65bb33228f6f25f0da6e29adfd9cafd1f7553f0
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-07-12 11:57:44 +0300

    Fixed products and steps rendering

--- src/Components/ViewRecipe/ViewRecipe.js
+++ src/Components/ViewRecipe/ViewRecipe.js
@@ -4,29 +4,33 @@ import "./style.css";
 class ViewRecipe extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      recipe: {}
+    };
   }
 
   componentDidMount() {
     const { recipe } = this.props.location.state;
+    this.setState({ recipe: recipe });
   }
 
   render() {
     console.log(this.recipe);
     return (
       <div>
-        <h1 />
-        <img src="" alt="" />
+        <h1>{this.state.recipe.recipeName}</h1>
+        <img src={this.state.recipe.dishImg} alt="Dish" />
         <ul>
-          <li>prod</li>
-          <li>prod</li>
-          <li>prod</li>
+          {this.state.recipe &&
+            this.state.recipe.ingredients &&
+            this.state.recipe.ingredients.map(ingredient => (
+              <li>{ingredient}</li>
+            ))}
         </ul>
         <ul>
-          <li>direction</li>
-          <li>direction</li>
-          <li>direction</li>
-          <li>direction</li>
-          <li>directio</li>
+          {this.state.recipe &&
+            this.state.recipe.directions &&
+            this.state.recipe.directions.map(direction => <li>{direction}</li>)}
         </ul>
       </div>
     );
