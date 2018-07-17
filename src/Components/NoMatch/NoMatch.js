95f2386b4cb19b989e3bba34c6889cc8b93c617e
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-07-17 11:34:41 +0300

    Centered 404 page info

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,10 +1,15 @@
 import React from "react";
+import notFound from "../../Images/404error.png";
+import "./style.css";
 
 const NoMatch = ({ location }) => (
-  <div>
-    <h3>
-      No match for <code>{location.pathname}</code>
-    </h3>
+  <div className="notFound-container">
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+      <img src={notFound} alt="404-not found" />
+    </div>
   </div>
 );
 
