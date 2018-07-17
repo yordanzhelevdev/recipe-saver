37b04610056cabd4be3359db47f60e9fc99b0bf8
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-07-17 17:51:36 +0300

    The login component will be only for login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,14 +5,22 @@ class Login extends React.Component {
   render() {
     return (
       <div className="auth-container">
-        <div className="login-form">
+        <div className="login-social">
           <h2>Login with:</h2>
           <button className="facebook-btn" />
         </div>
-        <div className="register-form">
+        <form className="login-form">
+          <label>Email</label>
+          <input type="email" />
+          <label>Password</label>
+          <input type="password" />
+        </form>
+        {/*
+       <div className="register-form">
           <h2>Register with:</h2>
           <button className="facebook-btn" />
         </div>
+       */}
       </div>
     );
   }
