1792a0d2752f0992aa345ea14d82ac95f528e55f
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-08-09 15:51:25 +0300

    Footer Added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './Footer.css';
+import githubImg from '../../Images/github.png';
 
 class Footer extends React.Component {
 	render() {
 		return(
 			<footer class="footer">
-				<span>Created with <span class="love">&#x1F496;</span>by Yordan Zhelev</span>
+				<span>Created with <span className="love" aria-label="heart emoticon">&#x1F496;</span>by <span>Yordan Zhelev</span></span>
+				<a href="https://github.com/yordanzhelevdev"><img src={githubImg} /></a>
 			</footer>
 		)
 	}
