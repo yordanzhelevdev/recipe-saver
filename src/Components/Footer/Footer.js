8afe9967f681c4fa7049e56445362a2432719456
Yordan Zhelev <yordanzhelevdev@gmail.com>
2018-08-10 17:45:11 +0300

    footer a tag warning fixed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,18 +1,35 @@
 import React from "react";
-import './Footer.css';
-import githubImg from '../../Images/github.png';
+import "./Footer.css";
+import githubImg from "../../Images/github.png";
 
 class Footer extends React.Component {
-	render() {
-		return (
-			<footer class="footer">
-				<div className="footer-content-wrapper">
-					<span>Created with <span className="love" role="img" aria-label="heart emoticon" alt="heart emoticon">&#x1F496;</span>by <span>Yordan Zhelev</span></span>
-					<a href="https://github.com/yordanzhelevdev" target="_blank"><img src={githubImg} /></a>
-				</div>
-			</footer>
-		)
-	}
+  render() {
+    return (
+      <footer class="footer">
+        <div className="footer-content-wrapper">
+          <span>
+            Created with{" "}
+            <span
+              className="love"
+              role="img"
+              aria-label="heart emoticon"
+              alt="heart emoticon"
+            >
+              &#x1F496;
+            </span>
+            by <span>Yordan Zhelev</span>
+          </span>
+          <a
+            href="https://github.com/yordanzhelevdev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubImg} />
+          </a>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
